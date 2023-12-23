5f4316b800722ece2cbb6a9a86a731519a5a5133
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-12-23 17:58:37 -0300

    iniciado desenvolvimento do Header da página

---
+++ guidelines/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+import Menu from './../MenuItem';   
+
+function Header(){
+    return(
+        <>
+            <img src='../../assets' />
+            <Menu />
+        </>
+    );
+}
+
+export default Header;
