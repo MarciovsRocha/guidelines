8aed161f8e522b9e2820c8057700545c22e45af5
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-12-23 17:59:10 -0300

    adicionado ícone para fechar a página, redefinidos margin

--- guidelines/src/pages/CardPage/index.js
+++ guidelines/src/pages/CardPage/index.js
@@ -1,11 +1,12 @@
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams, Navigate, Link } from 'react-router-dom';
 import './index.css';
 import documentos from '../../public_docs/public_docs'
+import CloseIconOverlay from '../../components/CloseIconOverlay';
 
 // TODO: transformar em um service
 function GetPageById(id){
   for (const key in documentos) {
-    if (documentos[key].id === Number(id)) {      
+    if (documentos[key].id === Number(id)) {
       return documentos[key];
     }
   }
@@ -21,13 +22,14 @@ function CardPage(){
   }
   console.log(page.url_header);
     return (
-        <div className="reading-page">            
-            <div className="image-crop">            
+        <div className="reading-page">
+            <CloseIconOverlay/>
+            <div className="image-crop">
             <img alt="three objects" className="reading-header" src={page.url_header} />
             </div>
             <h1>{ page.title }</h1>
-            <p className="prelude">{ page.prelude }</p>            
-            {page.section.map((s) => {                
+            <p className="prelude">{ page.prelude }</p>
+            {page.section.map((s) => {
                 return (
                 <div key={s.sub_title}>
                     <h3>{s.sub_title}</h3>
